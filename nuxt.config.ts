6536438371e129408d3ef2435566f73c229305b8
amorimweb <amorimwebdesign2023@gmail.com>
2026-03-02 02:19:03 -0300

    finalizacao - favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,14 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase', '@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt'],
+  app: {
+    head: {
+      title: 'Pebas Pro - Onde o talento encontra a oportunidade',
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon.png' }
+      ]
+    }
+  },
   components: [
     {
       path: '~/components',
