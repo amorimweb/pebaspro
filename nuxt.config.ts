90fec5d8f62a2019e5bf6f29d33539f9ab5c70c8
amorimweb <amorimwebdesign2023@gmail.com>
2026-03-04 00:29:40 -0300

    correcao favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,10 @@ export default defineNuxtConfig({
     head: {
       title: 'Pebas Pro - Onde o talento encontra a oportunidade',
       link: [
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
         { rel: 'icon', type: 'image/png', href: '/favicon.png' }
       ]
     }
